956876d99857ff08fccf0089ae8a21ef1299639b
sondereggern@bzz.ch <sondereggern@bzz.ch>
2024-04-16 09:39:40 +0200

    .

---
+++ 3.0/3.3.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const request = require('request');
+const {response} = require("express");
+const app = express();
+const port = 3000;
+
+function getTime() {
+    const response = new Date().toLocaleTimeString();
+    return response;
+}
+
+app.get('/now', (req, res) => {
+    const time = getTime();
+    res.send(`aktuele Zeit: ${time}`);
+});
+
+
+
+app.get('/zli', (req, res) => {
+    res.redirect('https://www.zli.ch/');
+});
+
+const names = ['Hans', 'Peter', 'Klaus', 'Kurt', 'Karl', 'Karlhe', 'Meier', 'Seppli', 'Hansli', 'Hanspeter', 'Hansmeier', 'Hanspetermeier', 'Hanspetermeierli', 'Hanspetermeierlisepp', 'Hanspetermeierliseppi', 'Hanspeter']
+
+app.get('/name', (req, res) => {
+    const radnom = names[Math.floor(Math.random() * names.length)]
+    res.send(`${radnom}`)
+});
+
+app.get('/html', (req, res) => {
+    res.sendFile("C:/Users/nicso/WebstormProjects/untitled4/3.0/index.html");
+});
+
+app.get('/image', (req, res) => {
+    res.sendFile( "C:/Users/nicso/Pictures/IMG_20221117_122941_685.jpg");
+});
+
+app.get('/teapot', (req, res) => {
+    res.status(418).send('I am a teapot');
+});
+
+app.get('/user-agent', (req, res) => {
+    res.send(req.headers['user-agent']);
+});
+
+app.get('/secret', (req, res) => {
+    res.status(403).send('Access denied');
+});
+
+app.get('/xml', (req, res) => {
+    res.set('Content-Type', 'text/xml');
+    res.send('<note><to>Tove</to><from>Jani</from><heading>Reminder</heading><body>Don\'t forget me this weekend!</body></note>');
+});
+
+app.get('/me', (req, res) => {
+    //sending a json object with age, name and email
+    res.json({
+        Alter: 21,
+        surname: 'Nic',
+        name: 'Sonderegger',
+        Wohnort: 'Zürich Rebenweg 30',
+        Augenfarbe: 'Blau',
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Server läuft auf Port ${port}`);
+});
+
